b8f5848bee374e378c565207b303ad3ea87a24d6
eslamabdallah74 <eslamabdallah301@gmail.com>
2023-04-16 01:53:57 +0200

    linked stack with example

--- DataStructures/LinkedList.php
+++ DataStructures/LinkedList.php
@@ -8,7 +8,8 @@ class Node
 {
     public $value;
     public $next;
-    public function __construct($value) {
+    public function __construct($value)
+    {
         $this->value = $value;
         $this->next = null;
     }
@@ -30,7 +31,8 @@ class LinkedList
         $this->head     = $newNode;
     }
 
-    public function pop() {
+    public function pop()
+    {
         if ($this->head === null) {
             throw new StackUnderflowException('Stack is empty, Cannot pop from empty stack!');
         }
@@ -39,13 +41,41 @@ class LinkedList
         return $value;
     }
 
-    function printLinkedListStack($stack) {
+    function printLinkedListStack($stack)
+    {
         $currentNode = $stack->head;
         while ($currentNode !== null) {
             echo "Stack is: [" .  $currentNode->value  . "] <br />";
             $currentNode = $currentNode->next;
         }
     }
-    
 
+    public function isEmpty()
+    {
+        return $this->head === null;
+    }
+
+    public function isExpressionWellFormed($expression)
+    {
+        $stack = new LinkedList();
+        for ($i = 0; $i < strlen($expression); $i++) {
+            $char = $expression[$i];
+            if ($char === "(" || $char === "[" || $char === "{") {
+                $stack->push($char);
+            } elseif ($char === ")" || $char === "]" || $char === "}") {
+                if ($stack->isEmpty()) {
+                    return false;
+                }
+                $lastBracket  = $stack->pop();
+                if ($char === ")" && $lastBracket !== "(") {
+                    return false;
+                } elseif ($char === "]" && $lastBracket !== "[") {
+                    return false;
+                } elseif ($char === "}" && $lastBracket !== "{") {
+                    return false;
+                }
+            }
+        }
+        return $stack->isEmpty();
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<form method="POST">
+    <label for="input">Enter a Expression to test if it's valid:</label>
+    <input type="text" placeholder="(4-2)+[2%2]" id="input" name="input">
+    <button type="submit">Submit</button>
+</form>
+
 <?php
 require_once 'autoload.php';
 
@@ -5,9 +11,15 @@ use Yomi\DataStructures\LinkedList;
 use Yomi\DataStructures\Node;
 
 
-$stack = new LinkedList();
-$stack->push('Eslam');
-$stack->push('Abdallah');
-$stack->push('abss');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input_value = $_POST["input"];
 
-$stack->printLinkedListStack($stack);
+    $stack       = new LinkedList();
+    $expression1 = $input_value;
+    $testExp     = $stack->isExpressionWellFormed($expression1);
+    if ($testExp) {
+        echo "Expression is well formatted";
+    } else {
+        echo "Expression is not well formatted";
+    }
+}
