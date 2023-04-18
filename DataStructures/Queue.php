73e765fd12cff078afd87dbf216a97df1f27aca7
eslamabdallah74 <eslamabdallah301@gmail.com>
2023-04-18 03:41:26 +0200

    Queue DataStructures

---
+++ DataStructures/Queue.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Yomi\DataStructures;
+
+use Yomi\DataStructures\Exceptions\StackUnderflowException;
+
+class Queue
+{
+    public $front;
+    public $back;
+    public $queueArray;
+    public $arrayLength;
+    public const MAX_LENGTH = 100;
+
+    public function __construct()
+    {
+        $this->front = 0;
+        $this->back = self::MAX_LENGTH - 1;
+        $this->queueArray = [];
+    }
+
+    public function isFull(): bool
+    {
+        return $this->arrayLength == self::MAX_LENGTH;
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->arrayLength == 0;
+    }
+
+    public function enQueue($element): void
+    {
+        if ($this->isFull()) {
+            throw new StackUnderflowException('Cannot add elements (array is full)');
+        } else {
+            $this->back = ($this->back + 1) % self::MAX_LENGTH;
+            $this->queueArray[$this->back] = $element;
+            $this->arrayLength++;
+        }
+    }
+
+    public function deQueue(): void
+    {
+        if ($this->isEmpty()) {
+            throw new StackUnderflowException('Cannot remove elements (array is empty)');
+        } else {
+            $this->front = ($this->front + 1) % self::MAX_LENGTH;
+            $this->arrayLength--;
+        }
+    }
+
+    public function getFront(&$front)
+    {
+        $front = $this->queueArray[$this->front];
+    }
+
+    public function getBack(&$back)
+    {
+        $back = $this->queueArray[$this->back];
+    }
+
+    public function print()
+    {
+        for ($i = $this->front; $i != ($this->back + 1) % self::MAX_LENGTH; $i = ($i + 1) % self::MAX_LENGTH) {
+            echo "Queue index number: " . $i . " Value is [" . $this->queueArray[$i] . "]";
+            echo "<br >";
+        }
+        // echo "Queue index number: " . $this->back . " Value is [" . $this->queueArray[$this->back] . "]";
+    }
+}
--- index.php
+++ index.php
@@ -1,25 +1,13 @@
-<form method="POST">
-    <label for="input">Enter a Expression to test if it's valid:</label>
-    <input type="text" placeholder="(4-2)+[2%2]" id="input" name="input">
-    <button type="submit">Submit</button>
-</form>
-
 <?php
 require_once 'autoload.php';
 
 use Yomi\DataStructures\LinkedList;
-use Yomi\DataStructures\Node;
+use Yomi\DataStructures\Queue;
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $input_value = $_POST["input"];
-
-    $stack       = new LinkedList();
-    $expression1 = $input_value;
-    $testExp     = $stack->isExpressionWellFormed($expression1);
-    if ($testExp) {
-        echo "Expression is well formatted";
-    } else {
-        echo "Expression is not well formatted";
-    }
-}
+$queue = new Queue();
+$queue->enQueue(4);
+$queue->enQueue(44);
+$queue->enQueue(444);
+$queue->enQueue(15);
+$queue->print();
